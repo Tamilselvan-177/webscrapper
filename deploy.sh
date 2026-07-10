f2204bcc88bb2976bf93b639442f7aff7c11bc88
Tamilselvan_A_K <231801177@rajalakshmi.edu.in>
2026-07-10 20:51:48 +0530

    Update deploy.sh to build frontend locally on server

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Pulling latest images from Docker Hub..."
 docker compose pull
 
 echo "Starting the application..."
-docker compose up -d
+docker compose up -d --build
 
 echo "Deployment successful! 🚀"
